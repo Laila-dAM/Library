9701bc5ecf5ab3989dde87fc109d87a567040738
Laila-dAM <lailamota.238@gmail.com>
2024-01-26 14:46:08 -0300

    Reorganizing the project

--- projeto.c
+++ projeto.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <locale.h>
-#define MAX_BOOKS 100
 
-struct Book {
-    char t[50];
-    char a[50];
-    int d;
-};
 
-struct library {
-    struct Book book[MAX_BOOKS];
-    int nl;
 
-    };
 
-    
 
